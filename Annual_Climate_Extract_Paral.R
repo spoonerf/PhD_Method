06b2f8edf48402160608c4450c867f2ff0146205
Fiona Spooner <spoonerfiona@gmail.com>
2016-03-01 11:06:15 +0000

    Converting daily climate data to annual and then parallel extracting the data with various sized buffers

---
+++ Annual_Climate_Extract_Paral.R
@@ -0,0 +1,45 @@
+install.packages("ncdf4")
+library(raster)
+library(ncdf4)
+
+rmean<-brick("tg_0.25deg_reg_v11.0.nc", varname = "tg")
+
+tm <- seq(as.Date('1950-01-01'), as.Date('2014-12-31'), 'day')
+s <- setZ(rmean, tm, 'days')
+
+tmy <- seq(as.Date('1950-01-01'), as.Date('2014-12-31'), 'year')
+
+
+rmn<-zApply(s, tmy, fun=mean, name='year')
+
+
+n<-8  #number of cores to use - not sure how many I can go up to
+cl<-makeCluster(n)
+registerDoParallel(cl) 
+
+yrs<-nlayers(rmn)
+step<-floor(yrs/n)
+
+ptime <- system.time({df<- foreach(lyr=seq(1,yrs,step)[1:n], .combine=cbind) %dopar%{
+  
+  rasterex <- raster:::extract(rpcp[[lyr:(lyr+step-1)]], xy_df, fun=mean, na.rm=TRUE)
+  raster10 <- raster:::extract(rpcp[[lyr:(lyr+step-1)]], xy_df, buffer=10000, fun=mean, na.rm=TRUE)
+  raster25 <- raster:::extract(rpcp[[lyr:(lyr+step-1)]], xy_df, buffer=25000, fun=mean, na.rm=TRUE)
+  raster50 <- raster:::extract(rpcp[[lyr:(lyr+step-1)]], xy_df, buffer=50000, fun=mean, na.rm=TRUE)
+  raster100 <- raster:::extract(rpcp[[lyr:(lyr+step-1)]], xy_df, buffer=100000, fun=mean, na.rm=TRUE)
+
+  }
+}) 
+ptime  
+
+stopCluster(cl)
+
+
+
+
+
+
+
+
+
+
