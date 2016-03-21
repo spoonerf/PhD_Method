1622a4b122b8100eb23fc206b1ab6f9ca6127e95
spoonerf <spoonerfiona@gmail.com>
2016-03-21 11:43:21 +0000

    Adding in Europe focussed data as "Europe_HILDA_5_year_pops.csv"

--- Global_Mixed_Effects.R
+++ Global_Mixed_Effects.R
@@ -4,13 +4,15 @@ LPI<-read.csv("LPI_populations_IP_fishedit_20140310_nonconf.csv")
 
 #pop<-read.csv("Global_Population_Trends_Rsq_Lambda_16_03_17.csv")
 pop<-read.csv("Global_Population_Trends_Rsq_Lambda_16_03_18.csv")
+EurHil<-read.csv("Europe_HILDA_5_year_pops.csv")
 
 head(pop)
 temp<-temp[,c("ID", "Estimate")]
 LPI<-LPI[,c("ID","Binomial","Common_name","Country","Region", "System", "Class","Specific_location", "Longitude", "Latitude")]
 
-
 df<-merge(merge(temp,luc, by="ID", all=TRUE), merge(LPI, pop, by="ID", all=TRUE),by="ID", all=TRUE)
+
+nrow(df)
 
 df2<-subset(df, !is.na(Estimate)&r_sq >= 0.5  & !is.na(LUC_dist)&length_time >=5 & System!="Marine" &Specific_location == 1)
 
@@ -24,7 +26,7 @@ sp_dups$loc_id<-1:length(sp_dups$Longitude)
 sp_dups_df<-merge(sp_dups, df2, by=c("Longitude","Latitude"))
 
 library(data.table)
-dt = as.data.table(sp_dups_df)
+#dt = as.data.table(sp_dups_df)
 
 parm_df<-sp_dups_df[,c("ID","Estimate", "LUC_dist")]  ##ID, land use, and climate
 
@@ -70,34 +72,59 @@ m1b_w = numeric(R)
 m1c_w = numeric(R)
 mnull_w = numeric(R)
 
-luc_est = numeric(R)
-clm_est = numeric(R)
-int_est = numeric(R)
+MTC_i = numeric(R)
+LUC_i = numeric(R)
+LUC_MTC_i = numeric(R)
 
-dt<-data.table(Euro)
+int_av = numeric(R)
+MTC_av = numeric(R)
+LUC_av = numeric(R)
+LUC_MTC_av = numeric(R)
+
+
+#dt<-data.table(Euro)
 
 for (i in 1:R) {
   dt2<-data.frame(dt[, ID[sample.int(.N, 1, TRUE)], by = loc_id])     #.N     signifies the number of rows when using data.table
   colnames(dt2)[2]<-"ID"
-  sp_dups_df2<-Euro[Euro$ID %in% dt2$ID,]
+  sp_dups_df2<-sp_df_scale[sp_df_scale$ID %in% dt2$ID,]
   
   m1<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+change_rate_scale:mean_slope_scale+(1|Binomial),data=sp_dups_df2, REML=F)
+  m1T<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+change_rate_scale:mean_slope_scale+(1|Binomial),data=sp_dups_df2)
   
   m1a<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+(1|Binomial),data=sp_dups_df2, REML=F)
+  m1aT<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+(1|Binomial),data=sp_dups_df2)
   
   m1b<-lmer(lambda_sum ~ change_rate_scale+(1|Binomial),data=sp_dups_df2, REML=F)
+  m1bT<-lmer(lambda_sum ~ change_rate_scale+(1|Binomial),data=sp_dups_df2)
   
   m1c<-lmer(lambda_sum ~ mean_slope_scale+(1|Binomial),data=sp_dups_df2, REML=F)
-  
+  m1cT<-lmer(lambda_sum ~ mean_slope_scale+(1|Binomial),data=sp_dups_df2)
+ 
   mnull<-lmer(lambda_sum ~ 1+(1|Binomial),data=sp_dups_df2, REML=F)
-#AIC
+  mnullT<-lmer(lambda_sum ~ 1+(1|Binomial),data=sp_dups_df2)
+  
+  #AIC
   AIC_m1[i]<-AIC(m1)
   AIC_m1a[i]<-AIC(m1a)
   AIC_m1b[i]<-AIC(m1b)
   AIC_m1c[i]<-AIC(m1c)
   AIC_mnull[i]<-AIC(mnull)
-#Rsq
-  models_list<-list(m1,m1a,m1b,m1c,mnull)
+
+  #Weights
+  
+  msAICc <- model.sel(m1,m1a,m1b,m1c,mnull)
+  msAICc$model<-rownames(msAICc)
+  msAICc<-data.frame(msAICc)
+  
+  m1_w[i]<-subset(msAICc, model=="m1")$weight
+  m1a_w[i]<-subset(msAICc, model=="m1a")$weight
+  m1b_w[i]<-subset(msAICc, model=="m1b")$weight
+  m1c_w[i]<-subset(msAICc, model=="m1c")$weight
+  mnull_w[i]<-subset(msAICc, model=="mnull")$weight
+  
+  #Rsq
+  models_list<-list(m1T,m1aT,m1bT,m1cT,mnullT)
   modelsR<-lapply(models_list,rsquared.glmm)
   modelsRsq <- matrix(unlist(modelsR), ncol=6, byrow=T)
   
@@ -111,25 +138,20 @@ for (i in 1:R) {
   cond_Rsq_m1c[i]<-modelsRsq[4,5]
   cond_Rsq_mnull[i]<-modelsRsq[5,5]
 
-#Weights
-
-  msAICc <- model.sel(m1,m1a,m1b,m1c,mnull)
-  msAICc$model<-rownames(msAICc)
-  msAICc<-data.frame(msAICc)
-  
-  m1_w[i]<-subset(msAICc, model=="m1")$weight
-  m1a_w[i]<-subset(msAICc, model=="m1a")$weight
-  m1b_w[i]<-subset(msAICc, model=="m1b")$weight
-  m1c_w[i]<-subset(msAICc, model=="m1c")$weight
-  mnull_w[i]<-subset(msAICc, model=="mnull")$weight
   
   #estimates from most complex model
-  
-  luc_est[i]<-fixef(m1)["change_rate_scale"]
-  clm_est[i]<-fixef(m1)["mean_slope_scale"]
-  int_est[i]<-fixef(m1)["change_rate_scale:mean_slope_scale"]
-  print(i)
+  var_imp<-summary(model.avg(models_list))
+  MTC_i[i]<-var_imp$importance["mean_slope_scale"]
+  LUC_i[i]<-var_imp$importance["change_rate_scale"]
+  LUC_MTC_i[i]<-var_imp$importance["change_rate_scale:mean_slope_scale"]
+  #BM_i[i]<-var_imp$importance["Bodymass"]
 
+  int_av[i]<-var_imp$coefmat.subset["(Intercept)","Estimate"]
+  MTC_av[i]<-var_imp$coefmat.subset["mean_slope_scale","Estimate"]
+  LUC_av[i]<-var_imp$coefmat.subset["change_rate_scale","Estimate"]
+  LUC_MTC_av[i]<-var_imp$coefmat.subset["change_rate_scale:mean_slope_scale","Estimate"]
+  
+  print(i)
   }
 
 
@@ -139,18 +161,6 @@ AIC_del<-AIC_df[,c(1:4)] - AIC_df$AIC_mnull
 
 colMeans(AIC_del)
 
-
-hist(luc_est)
-hist(clm_est)
-hist(int_est)
-
-luc_est_glb<-luc_est
-clm_est_glb<-clm_est
-int_est_glb<-int_est
-
-luc_est_hil<-luc_est
-clm_est_hil<-clm_est
-int_est_hil<-int_est
 
 mean(marg_Rsq_m1)
 mean(marg_Rsq_m1a)
@@ -169,6 +179,11 @@ mean(m1b_w)
 mean(m1c_w)
 mean(mnull_w)
 
+
+
+
+
+#############
 Euro<-Inc5MammalsTemp[!(Inc5MammalsTemp$ID %in% sp_df_scale$ID),] 
 Euro<-subset(sp_df_scale, Region=="Europe")
 Afr<-subset(sp_df_scale, Region=="Africa")
@@ -213,44 +228,6 @@ sp_dups<-data.frame(ddply(Inc5MammalsTemp,.(Longitude,Latitude),nrow))
 sp_dups$loc_id<-1:length(sp_dups$Longitude)
 sp_dups_df<-merge(sp_dups, Inc5MammalsTemp, by=c("Longitude","Latitude"))
 
-####counting the number of mammals or birds at each location
-
-result<-data.frame()
-
-for (i in 1:length(sp_dups$loc_id)){
-  
-  sub<-subset(sp_dups_df, loc_id==i)
-  bird_loc<-sum(sub$Class == "Aves")
-  mamm_loc<-sum(sub$Class == "Mammalia")
-  amph_loc<-sum(sub$Class == "Amphibia")
-  count_loc<-cbind(i,bird_loc, mamm_loc,amph_loc)
-  print(count_loc)
-  result<-rbind(count_loc,result)
-}
-
-colnames(result)[1]<-"loc_id"
-
-sp_dups_df2<-merge(sp_dups_df, result, by="loc_id")
-
-###bootstrapping
-
-library(data.table)
-dt = as.data.table(sp_dups_df)
-
-parm_df<-sp_dups_df[,c("ID","MnSlope50k", "mean_res_quart",  "change_rate_49")]  ##ID, land use, and climate
-
-parm_mat<-as.matrix(parm_df)
-parm_scale<-scale(parm_mat[,c("MnSlope50k", "mean_res_quart", "change_rate_49")])       #use the scaling factors at the bottom of these to scale the rasters
-
-parm_id<-parm_mat[,"ID"]
-
-parm_df_scale_inc<-data.frame(parm_id,parm_scale)
-
-colnames(parm_df_scale_inc)<-c("ID","mean_slope_scale", "mean_var_scale", "change_rate_scale")
-
-sp_df_scale_inc<-merge(sp_dups_df, parm_df_scale_inc, by="ID")
-
-Euro_Inc<-sp_df_scale_inc[sp_df_scale_inc$ID %in% sp_df_scale$ID,] 
 
 minti<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+change_rate_scale:mean_slope_scale+(1|Binomial),data=Euro_Inc, REML=F)
 maddi<-lmer(lambda_sum ~ change_rate_scale+mean_slope_scale+(1|Binomial),data=Euro_Inc, REML=F)
