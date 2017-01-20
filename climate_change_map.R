efa89702fcdb851274457f14c3616e4cb724aea9
spoonerf <spoonerfiona@gmail.com>
2017-01-20 17:40:03 +0000

    Better faster method using stackApply and calc

--- climate_change_map.R
+++ climate_change_map.R
@@ -10,20 +10,26 @@ CR00s<-brick("cru_ts3.23.2001.2010.tmp.dat.nc")
 
 CR<-stack(CR40s[[109:120]],CR50s,CR60s,CR70s,CR80s,CR90s,CR00s[[c(1:60)]])
 
-Jan<-seq(1, nlayers(CR), 12)
-Dec<-Jan+11
+year_mon<-rep(1:(nlayers(CR)/12), each=12)
 
-year_s<-stack()
-
-for (i in 1:length(Jan)){
-  
-  year<-mean(CR[[Jan[i]:Dec[i]]])
-  year_s<-stack(year, year_s)
-   
-}
+#changing from monthly to annual data
+year_s<-stackApply(CR, indices=year_mon, fun=mean, na.rm=TRUE)
 
 years<-1950:2005
 names(year_s)<-years
+
+X <- cbind(1, years)
+
+## pre-computing constant part of least squares
+invXtX <- solve(t(X) %*% X) %*% t(X)
+
+## much reduced regression model; [2] is to get the slope
+quickfun <- function(y) (invXtX %*% y)[2]
+x4 <- calc(year_s, quickfun) 
+
+writeRaster(x4, "Global_Rate_Mean_Temp_Change.tif")
+
+
 
 ##plotting each year
 # for (i in 1:nlayers(year_s)){
