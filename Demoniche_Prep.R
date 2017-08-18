59edced2366ac0c32df28f3dec6afc1402884bd3
spoonerf <spoonerfiona@gmail.com>
2017-08-18 11:02:24 +0100

    More streamlined code for biomod2 and demoniche - creates sdm for each year 1950-2016 but with same location point data and unresearched parameters for dispersal etc

--- Demoniche_Prep.R
+++ Demoniche_Prep.R
@@ -31,10 +31,10 @@ for (i in 1:length(years)){
   }
 
 #########Creating a biomod2 SDM
-library(biomod2)
 library(dismo)
+library(demoniche)
+library(biomod2)
 
-#getting location points for capra ibex from gbif and then cleaning this data
 capra <-gbif("capra", "ibex", geo=FALSE)
 capgeo <- subset(capra, !is.na(lon) & !is.na(lat)) 
 dups <- duplicated(capgeo[, c("lon", "lat")])
@@ -47,195 +47,93 @@ xy<-as.matrix(capc[,c(1,2)])
 df<-data.frame(capg2$presence)
 sp<-SpatialPointsDataFrame(coords=xy, data=df)
 
-# eval<-capg2[sample(1:nrow(capg2),sampleSize),]
-# test<-capg2[!capg2$ID %in% eval$ID,]
-# 
-# capc<-as.matrix(test[ , c( "lon","lat", "presence")])
-# xy<-as.matrix(capc[,c(1,2)])
-# df<-data.frame(test$presence)
-# 
-# cap_eval<-as.matrix(eval[ , c( "lon","lat", "presence")])
-# xy_eval<-as.matrix(cap_eval[,c(1,2)])
-# df_eval<-data.frame(eval$presence)
-# 
-# 
-# sp_test<-SpatialPointsDataFrame(coords=xy, data=df)
-# sp_eval<-SpatialPointsDataFrame(coords=xy_eval, data=df_eval)
-
-bio_1950<-stack("C:/Users/Fiona/Documents/PhD/PhD_Method/Bioclim/1950_bioclim_variable_stack.tif")
-bio_2016<-stack("C:/Users/Fiona/Documents/PhD/PhD_Method/Bioclim/2016_bioclim_variable_stack.tif")
-
+years<-as.character(1950:2016)
 e<-extent(sp)+4
-
-alps<-crop(bio_1950,e)
-alps_now<-crop(bio_2016,e)
-plot(alps[[1]])
-plot(alps_now[[1]])
-alps<-stack(alps)
-alps_now<-stack(alps_now)
-
 species<-"Capra ibex"
-
-
-my_biomod_data<-BIOMOD_FormatingData(resp.var = sp,
-                     expl.var =  alps_now,  #alps for 1950, alps now for 2016
-                     #eval.resp.var = sp_eval,
-                     #eval.expl.var = bio_1950,
-                     resp.name = "Capra_ibex",
-                     PA.nb.rep = 10,    #switch these three off to run without pseudo absences
-                     PA.nb.absences = 10,
-                     PA.strategy = 'random',
-                     na.rm=TRUE)
-
-#plot(my_biomod_data)
 
 myBiomodOption <- BIOMOD_ModelingOptions()
 
-myBiomodModelOut <- BIOMOD_Modeling(
-  my_biomod_data,
-  models = c("GLM", "GAM", "RF"),
-  models.options = myBiomodOption,
-  NbRunEval=3,
-  DataSplit=80,
-  Prevalence=0.5,
-  VarImport=3,
-  models.eval.meth = c('TSS','ROC'),
-  SaveObj = TRUE,
-  rescal.all.models = TRUE,
-  do.full.models = FALSE,
-  modeling.id = paste(species,"FirstModeling",sep=""))
 
-myBiomodModelEval <- get_evaluations(myBiomodModelOut)
+for (i in 1:length(years)){
+  
+  bio<-stack(paste("C:/Users/Fiona/Documents/PhD/PhD_Method/Bioclim/",years[i],"_bioclim_variable_stack.tif", sep=""))  
+  alps<-crop(bio,e)  
+  alps<-stack(alps)
+  my_biomod_data<-BIOMOD_FormatingData(resp.var = sp,
+                                       expl.var =  alps,  #alps for 1950, alps now for 2016
+                                       #eval.resp.var = sp_eval,
+                                       #eval.expl.var = bio_1950,
+                                       resp.name = "Capra_ibex",
+                                       PA.nb.rep = 10,    #switch these three off to run without pseudo absences
+                                       PA.nb.absences = 10,
+                                       PA.strategy = 'random',
+                                       na.rm=TRUE)
+  
+  myBiomodModelOut <- BIOMOD_Modeling(
+    my_biomod_data,
+    models = c("GLM", "GAM", "RF"),
+    models.options = myBiomodOption,
+    NbRunEval=3,
+    DataSplit=80,
+    Prevalence=0.5,
+    VarImport=3,
+    models.eval.meth = c('TSS','ROC'),
+    SaveObj = TRUE,
+    rescal.all.models = TRUE,
+    do.full.models = FALSE,
+    modeling.id = paste(species,"FirstModeling",sep=""))
+  
+  myBiomodEM <- BIOMOD_EnsembleModeling(
+    modeling.output = myBiomodModelOut,
+    chosen.models = 'all',
+    em.by='all',
+    eval.metric = c('TSS'),
+    eval.metric.quality.threshold = c(0.7),
+    prob.mean = T,
+    prob.cv = T,
+    prob.ci = T,
+    prob.ci.alpha = 0.05,
+    prob.median = T,
+    committee.averaging = T,
+    prob.mean.weight = T,
+    prob.mean.weight.decay = 'proportional' )
+  
+  myBiomodProj <- BIOMOD_Projection(
+    modeling.output = myBiomodModelOut,
+    new.env = alps,    #alps for 1950 and alps now for 2016
+    proj.name = 'current',
+    selected.models = 'all',
+    binary.meth = 'TSS',
+    compress = 'xz',
+    clamping.mask = F,
+    output.format = '.grd')
+  
+  myCurrentProj <- get_predictions(myBiomodProj)
+  
+  ave_proj<-mean(myCurrentProj)
+  writeRaster(ave_proj, paste("C:/Users/Fiona/Documents/PhD/PhD_Method/Alp_SDMs/", years[i],"_SDM.tif", sep=""), overwrite=TRUE)
+  print(years[i])
+}
 
+#############################
 
-dimnames(myBiomodModelEval)
-
-myBiomodModelEval["TSS","Testing.data","RF",,]
-
-myBiomodModelEval["ROC","Testing.data",,,]
-
-get_variables_importance(myBiomodModelOut)
-
-
-myBiomodEM <- BIOMOD_EnsembleModeling(
-  modeling.output = myBiomodModelOut,
-  chosen.models = 'all',
-  em.by='all',
-  eval.metric = c('TSS'),
-  eval.metric.quality.threshold = c(0.7),
-  prob.mean = T,
-  prob.cv = T,
-  prob.ci = T,
-  prob.ci.alpha = 0.05,
-  prob.median = T,
-  committee.averaging = T,
-  prob.mean.weight = T,
-  prob.mean.weight.decay = 'proportional' )
-
-myBiomodEM 
-
-get_evaluations(myBiomodEM)
-
-#pres.only.eval <- BIOMOD_presenceonly(myBiomodModelOut, myBiomodEM)
-
-myBiomodProj <- BIOMOD_Projection(
-  modeling.output = myBiomodModelOut,
-  new.env = alps_now,    #alps for 1950 and alps now for 2016
-  proj.name = 'current',
-  selected.models = 'all',
-  binary.meth = 'TSS',
-  compress = 'xz',
-  clamping.mask = F,
-  output.format = '.grd')
-
-
-myBiomodProj
-
-
-list.files("Capra.ibex/proj_current/")
-
-
-plot(myBiomodProj, str.grep="RF")
-
-myCurrentProj <- get_predictions(myBiomodProj)
-
-ave_proj<-mean(myCurrentProj)
-
-old<-raster("Average_SDM_1950.tif")
-
-plot(ave_proj)
-
-plot(myCurrentProj[[11]])
-
-test<-myCurrentProj[[11]]/10
-
-writeRaster(ave_proj, "Average_SDM_2016.tif") #glm, gam, rf
-
-
-writeRaster(test, "Example_biomod_SDM.tif", overwrite=T)#3
-writeRaster(test, "Example_biomod_SDM_loss.tif", overwrite=T)#11
-
-test[test<50]<-0
-test[test>50]<-1
-plot(test)
-
-
-writeRaster(test, "Example_presence_absence_biomod_SDM.tif", overwrite=T)
-writeRaster(test, "Example_presence_absence_biomod_loss_SDM.tif", overwrite=T)
-
-############## Demoniche
-install.packages("popbio")
-install.packages("demoniche", repos="http://R-Forge.R-project.org")
 library(demoniche)
-
-sdm<-raster("Average_SDM_1950.tif")
-sdm_loss<-raster("Average_SDM_2016.tif")
-sdm_table<-as.data.frame(sdm, xy = TRUE)
-sdm_loss_table<-as.data.frame(sdm_loss, xy = TRUE)
-
-dir<-getwd()
-load(paste(dir, "COMADRE_v.1.0.0.RData", sep="/"))
-
+library(rgdal)
+library(raster)
 species<-"Capra_ibex"
 
-tempMetadata<-subset(comadre$metadata, SpeciesAccepted==species)
-
-keep<-as.numeric(rownames(tempMetadata))
-
-tempMat<-comadre$mat[keep]   #MatA is matrix pop model, can be split into U, F and/or C
-
-MatList<-list(tempMat[[1]][[1]])  #varies depending on number of matrices - need to find a way to code this better
-AllMat<-unlist(MatList)
-matrices<-matrix(AllMat, ncol=length(MatList))
-colnames(matrices)<- c("Reference_matrix")
-
-#setting up
-
-pyr<-subset(df2, Binomial ==species)    #record 11470 had wrong longitude - in Russia!
+df2<-read.csv("LPI_pops_20160523_edited.csv")
+pyr<-subset(df2, Binomial ==species & Specific_location==1)    #record 11470 had wrong longitude - in Russia!
 
 plot(pyr$Longitude, pyr$Latitude)
 
-pyrs<-pyr[,c("ID","Longitude","Latitude","lambda_mean")]
+pyrs<-pyr[,c("ID","Longitude","Latitude")]
 
-id<-pyrs$ID
-#lam<-as.numeric(pyrs$lambda_mean)
+id<-pyrs$ID*100
 lam<-rep(200,length(id))    #not sure what the value here pertains to - think it sets starting population so should use values from LPI?
-
-
-library(rgdal)
-
 pyrxy<-SpatialPoints(pyr[,c("Longitude","Latitude")])
 
-library(raster)
-
-# e<-extent(pyrxy)
-# xmn<-floor(e[1])
-# xmx<-ceiling(e[2])
-# ymn<-floor(e[3])
-# ymx<-ceiling(e[4])
-
-#e2<-extent(xmn,xmx,ymn,ymx)
-
+sdm<-raster("C:/Users/Fiona/Documents/PhD/PhD_Method/Alp_SDMs/1950_SDM.tif")
 e2<-extent(sdm)
 
 r<-raster(e2, resolution=res(sdm))
@@ -253,28 +151,60 @@ ridm<-as.vector(rid)
 df<-data.frame(ridm,rz_spdf,rzm)
 colnames(df)<-c( "PatchID","X","Y","area")
 
-Populations<-data.frame(na.omit(df))  
+Populations<-data.frame(na.omit(df)) 
 
-#linking up the sdm map cells with the population cells
-sdm[is.na(sdm)] <- 0
-sdm_loss[is.na(sdm_loss)] <- 0
 
-sdm_table<-as.data.frame(sdm, xy = TRUE)
-sdm_loss_table<-as.data.frame(sdm_loss, xy = TRUE)
+sdm_df<-data.frame(ID=1:2030)
 
-sdm_table$ID<-1:length(sdm_table$ID)
-sdm_table$ID[which(!is.na(df$PatchID))]<-df$PatchID[!is.na(df$PatchID)]
+#formatting data for demoniche
+for (i in 1:length(years)){
+  
+  sdm<-raster(paste("C:/Users/Fiona/Documents/PhD/PhD_Method/Alp_SDMs/", years[i],"_SDM.tif", sep=""))  
+  if (i ==1){
+    vec<-as.data.frame(sdm, xy = TRUE)      
+  } else{
+    vec<-as.data.frame(sdm)  
+  }
+  
+  sdm_df<-cbind(sdm_df, vec)
+  print(i)
+}
 
-niche_map_mine<-cbind(sdm_table[,c(4,1,2,3)],sdm_loss_table$Average_SDM_2016)
-colnames(niche_map_mine)<-c("gridID", "X", "Y", "Year_1950", "Year_2016")
+sdm_df$ID[which(!is.na(df$PatchID))]<-df$PatchID[!is.na(df$PatchID)]
+
+niche_map_mine<-sdm_df
+colnames(niche_map_mine)[1:3]<-c("gridID", "X", "Y")
+
+col_years<-paste("Year_", 1950:2016, sep="")
+colnames(niche_map_mine)[4:length(colnames(niche_map_mine))]<-col_years
 
 niche_formulas <- as.formula(paste(paste(colnames(niche_map_mine)[-c(1:3)], collapse="+"),"X+Y",sep="~"))
 
 print(levelplot(niche_formulas, niche_map_mine, col.regions=rev(heat.colors(100)), main = "Niche Values"))
 
-no_yrs_mine<-33 #number of years each time period represents - could be 1 when I do it for reals
+no_yrs_mine<-1 #number of years each time period represents - could be 1 when I do it for reals
+
 
 ####matrix set up
+
+dir<-getwd()
+load(paste(dir, "COMADRE_v.1.0.0.RData", sep="/"))
+
+species<-"Capra_ibex"
+
+tempMetadata<-subset(comadre$metadata, SpeciesAccepted==species)
+
+keep<-as.numeric(rownames(tempMetadata))
+
+tempMat<-comadre$mat[keep]   #MatA is matrix pop model, can be split into U, F and/or C
+
+MatList<-list(tempMat[[1]][[1]])  #varies depending on number of matrices - need to find a way to code this better
+AllMat<-unlist(MatList)
+matrices<-matrix(AllMat, ncol=length(MatList))
+colnames(matrices)<- c("Reference_matrix")
+
+
+
 prob_scenario<-c(0.5,0.5)    #need to check this
 
 noise<-0.95     #need to check this
@@ -308,25 +238,21 @@ proportion_initial<- rep(1/length(stages), length(stages)) #proportion of popula
 
 density_individuals <- 1   #also compulsory not sure what best value would be
 
-K<-1000   #carrying capacity
- 
+K<-10000   #carrying capacity
+
 K_weight<-c(0, 1.5,1,1,1,1,1,1)  #the weight with which carrying capacity affects each stage was FALSE
 
 fraction_SDD <- 0.05  #short distance dispersal
 
 dispersal_constants_mine <-c(0.7,0.7,0.1,3)
 
-#fraction_LDD_mine <- 0.05 #long distance dispersal
-##Populations - prioritise sorting this out
-  
-niche_map_mine2<-c("period_A", "period_B")  
-
+niche_map_mine[is.na(niche_map_mine)]<-0
 
 demoniche_setup(modelname = "Capra_ibex",Populations = Populations, Nichemap = niche_map_mine,
                 matrices = matrices,matrices_var = matrices_var, prob_scenario = prob_scenario,
                 stages = stages, proportion_initial = proportion_initial,
                 density_individuals = density_individuals,
-                fraction_LDD = 0.5, fraction_SDD = 0.5,
+                fraction_LDD = 0.05, fraction_SDD = 0.05,
                 dispersal_constants = dispersal_constants_mine,
                 transition_affected_niche = transition_affected_niche,
                 transition_affected_demogr = transition_affected_demogr,
@@ -335,30 +261,29 @@ demoniche_setup(modelname = "Capra_ibex",Populations = Populations, Nichemap = n
                 no_yrs = no_yrs_mine, K=K, Kweight = K_weight, 
                 sumweight =sumweight)
 
+RPyran_niche <- demoniche_model(modelname = "Capra_ibex", Niche = TRUE, 
+                                Dispersal = FALSE, repetitions = 1,
+                                foldername = "RPyran_minimal")
 
-#works and is simple - exponential increase
-#demoniche_setup(modelname = "Capra_ibex",Populations = Populations, 
-#                matrices_var = matrices_var,matrices = matrices,
-#                 stages = stages, proportion_initial = proportion_initial,
-#                density_individuals = density_individuals,
-#                no_yrs = 80, sumweight =sumweight)
-
-
-#important to include sumweight, I think the default is FALSE but that 
-#causes the population to be 0 in all years
-
-RPyran_min_run <- demoniche_model(modelname = "Capra_ibex", Niche = TRUE, 
-                                  Dispersal = FALSE, repetitions = 1,
-                                  foldername = "RPyran_minimal")
-
-RPyran_min_run[,"Meanpop","Reference_matrix"]
+RPyran_niche[,"Meanpop","Reference_matrix"]
 
 years<-1950:2016
-plot(RPyran_min_run[,"Meanpop","Reference_matrix"])
+plot(RPyran_niche[,"Meanpop","Reference_matrix"])
 
-bleh<-cbind(years,RPyran_min_run[,"Meanpop","Reference_matrix"])
+bleh<-cbind(years,RPyran_niche[,"Meanpop","Reference_matrix"])
 plot(bleh)
 
+RPyran_disp_niche <- demoniche_model(modelname = "Capra_ibex", Niche = TRUE, 
+                                     Dispersal = TRUE, repetitions = 1,
+                                     foldername = "RPyran_disp_niche")
+
+RPyran_disp_niche[,"Meanpop","Reference_matrix"]
+
+years<-1950:2016
+#plot(RPyran_disp_niche[,"Meanpop","Reference_matrix"])
+
+bleh<-cbind(years,RPyran_disp_niche[,"Meanpop","Reference_matrix"], RPyran_min_run[,"Max","Reference_matrix"])
+plot(bleh, type="l")
 
 
 
