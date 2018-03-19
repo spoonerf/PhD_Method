103664816134000d6aa0335a56f3863de9a687df
spoonerf <spoonerfiona@gmail.com>
2018-03-19 12:11:18 +0000

    dispersal changed to include all stages

--- demoniche_dispersal_me.R
+++ demoniche_dispersal_me.R
@@ -18,7 +18,7 @@ demoniche_dispersal_me<-function (seeds_per_population, fraction_LDD, fraction_S
     source_patches <- which(colSums(seeds_per_population_migrate_SDD) >
                               0)
     for (px_orig in source_patches) {
-      for (pxdisp_new in 1:length(seeds_per_population_migrate_SDD)) {
+      for (pxdisp_new in 1:length(seeds_per_population_migrate_SDD[1,])) {
         if (dist_latlong[pxdisp_new, px_orig] == neigh_index[1]) {
           # seeds_per_population_new_SDD[pxdisp_new] <- seeds_per_population_new_SDD[pxdisp_new] + 
           #   (seeds_per_population_migrate_SDD[px_orig] * 
@@ -57,5 +57,4 @@ demoniche_dispersal_me<-function (seeds_per_population, fraction_LDD, fraction_S
   #seeds_total<-niche_values*(seeds_stay + seeds_per_population_new_SDD + seeds_per_population_new_LDD)
   return(seeds_stay + seeds_per_population_new_SDD + seeds_per_population_new_LDD)
   #return(seeds_stay)
->>>>>>> 6350399a964b1683bd7f56a1c7b22069732e8720
 }
