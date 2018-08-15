75f217fe04b8e9b491cfeb4ce0ef06bcf1ea32b1
spoonerf <spoonerfiona@gmail.com>
2018-08-15 13:25:29 +0100

    dispersal works now (fingers crossed)

--- dispersal_faster_csv.R
+++ dispersal_faster_csv.R
@@ -49,14 +49,14 @@ demoniche_dispersal_csv<-function (seeds_per_population, fraction_LDD, fraction_
     
     disp_prob<-dispersal_probabilities
     
-    sample_ages<-function(x){
+    sample_ages<-function(x, dp){
       new_patches<-sample(1:length(dp),x,prob=dp, replace =TRUE)
       return(new_patches)
     }
     
-    disp_out<-function(stg,seeds_new){
+    disp_out<-function(stg,seeds_new, stage_out){
       
-      a<-data.frame(table(stage_out[[stg]]))
+      a<-data.frame(table(stage_out[[stg]])) #slowness
       a$Var1<-as.numeric(as.character(a$Var1))
       a$Freq<-as.numeric(as.character(a$Freq))
       
@@ -69,16 +69,17 @@ demoniche_dispersal_csv<-function (seeds_per_population, fraction_LDD, fraction_
     }
     
    
-    dispersal<-function(j){
+    dispersal<-function(j, seeds_ldd, disp_prob){
       
       dp<-disp_prob[j,]
-      ages<-matrix(seeds_per_population_migrate_LDD[,j], ncol = 1)
-      stage_out<-lapply(ages, sample_ages)
+      print(j)
+      ages<-matrix(seeds_ldd[,j], ncol = 1)
+      stage_out<-lapply(X = ages, FUN = sample_ages, dp = dp )
       st<-as.matrix(1:length(stage_out))
       
       if(length(stage_out)>0){
-        seeds_new_out<-sapply( X = st,FUN  =  disp_out,  seeds_new =  seeds_new)
-        seeds_newt<-t(seeds_new_out)
+        seeds_new_out<-sapply( X = st,FUN  =  disp_out,  seeds_new =  seeds_new, stage_out = stage_out)
+        #seeds_newt<-t(seeds_new_out)
         
         return(seeds_new_out)
         
@@ -93,10 +94,15 @@ demoniche_dispersal_csv<-function (seeds_per_population, fraction_LDD, fraction_
     
     source_patches<-as.matrix(source_patches_ldd, ncol = 1)
     
-    check_disp<-apply(X = source_patches,1, FUN = dispersal)
+    check_disp<-apply(X = source_patches,1, FUN = dispersal, seeds_ldd = seeds_per_population_migrate_LDD, disp_prob = disp_prob)
     
-    seeds_per_population_new_LDD<-matrix(check_disp, ncol = ncol(seeds_per_population_new_LDD) , nrow = length(stages), byrow = T)
-  }
+    seeds_per_population_new_LDD<-matrix(rowSums(check_disp), ncol = ncol(seeds_per_population_new_LDD) , nrow = length(stages), byrow = T)
+    
+    print(paste("no. source_patches ", length(source_patches_ldd), sep=""))
+    print(paste("no. to migrate ",sum(colSums(seeds_per_population_migrate_LDD)), sep=""))
+    print(paste("no. which migrated ", sum(colSums(seeds_per_population_new_LDD)), sep=""))
+    
+    }
       
 
   seeds_stay <- (seeds_per_population - seeds_per_population_migrate_SDD -   #seeds that migrate must go out of the cell and are taken off the total
